f2c1b2dee4a8d2f01e11951b117688ee0810637c
hussu97 <h_abbasi97@hotmail.com>
2018-05-04 11:00:06 +0400

    edited code

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 monitored_folder = 'C:\\Users\\H_Abb\\Desktop\\Space_Hackathon'
-mag, tempir, uvlight, magx, magy, magz,dist,orientx,orienty = [], [], [], [], [], [],[],[],[]
+mag, tempir, uvlight, magx, magy, magz,dist,orientx,orienty,orientz = [], [], [], [], [], [],[],[],[],[]
 
 MAG_BASE = 3.22389238
 TEMP_IR_BASE = 25.3
@@ -29,50 +29,33 @@ def checkUVLight():
     if(len(tempir) > 5 and ((sum(uvlight[-5:]) / 5) - UV_LIGHT_INDEX_BASE) / UV_LIGHT_INDEX_BASE > 0.01):
         print('Object detected')
 
-def lineGraph(xlabel,ylabel,title,X,y,xlimi,xlimf,ylimi,ylimf):
+def lineGraph(xlabel,ylabel,title,X,y):
     plt.scatter(X,y,color='red')
     plt.plot(X,y,color = 'blue')
-    plt.gca().set_ylim([ylimi,ylimf])
     plt.title(title)
+    plt.xlim(170,200)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.show()
 
 
-class FolderMonitor(FileSystemEventHandler):
-    def on_modified(self, event):
-        print(event)
-        dataset = pd.read_csv('data.csv')
-        magx.append(dataset.iloc[-1:, :1].values.tolist()[0][0])
-        magy.append(dataset.iloc[-1:, 1:2].values.tolist()[0][0])
-        magz.append(dataset.iloc[-1:, 2:3].values.tolist()[0][0])
-        tempir.append(dataset.iloc[-1:, 3:4].values.tolist()[0][0])
-        uvlight.append(dataset.iloc[-1:, 4:5].values.tolist()[0][0])
-        orientx.append(dataset.iloc[-1:, 5:6].values.tolist()[0][0])
-        orienty.append(dataset.iloc[-1:, 6:7].values.tolist()[0][0])
-        for x, y, z in zip(magx, magy, magz):
-            mag.append(sqrt(x * x + y * y + z * z))
-        for x, y in zip(orientx,orienty):
-            orientation = sqrt(x*x+y*y)
-            dist.append((orientation/360)*2*pi*(MARS_RADIUS+DISTANCE_FROM_SURFACE))
-            print(dist)
-        del mag[len(dataset):]
-        del tempir[len(dataset):]
-        del uvlight[len(dataset):]
-        del dist[len(dataset):]
-        if(len(tempir) > 5 and (abs((sum(tempir[-5:]) / 5) - TEMP_IR_BASE) / TEMP_IR_BASE) > 0.02):
-            checkMag()
-            checkUVLight()
-        if(len(dist)>5):
-            print('hi')
-            lineGraph('Distance (m)','Temperature (C)','IR output',dist,tempir)
-
-
-observer = Observer()
-observer.schedule(FolderMonitor(), monitored_folder, recursive=True)
-observer.start()
-try:
-    time.sleep(1)
-except KeyboardInterrupt:
-    observer.stop()
-observer.join()
+dataset = pd.read_csv('data.csv')
+magx.append(dataset.iloc[:, 3:4].values.tolist()[0])
+magy.append(dataset.iloc[:, 4:5].values.tolist()[0])
+magz.append(dataset.iloc[:, 5:6].values.tolist()[0])
+tempir.append(dataset.iloc[:, 1:2].values.tolist()[0])
+uvlight.append(dataset.iloc[:, 2:3].values.tolist()[0])
+orientx.append(dataset.iloc[:, 6:7].values.tolist()[0])
+orienty.append(dataset.iloc[:, 7:8].values.tolist()[0])
+orientz.append(dataset.iloc[:, 8:9].values.tolist()[0])
+for x, y, z in zip(magx, magy, magz):
+    mag.append(sqrt(x * x + y * y + z * z))
+for x, y,z in zip(orientx,orienty,orientz):
+    orientation = sqrt(x*x+y*y+z*z)
+    dist.append(x)
+lineGraph('Distance (m)','Temperature (C)','IR output',dist,tempir)
+lineGraph('Distance (m)','UV Index (mW/cm^2)','UV output',dist,uvlight)
+for i in range(len(tempir)):
+    if((abs((sum(tempir[-5:]) / 5) - TEMP_IR_BASE) / TEMP_IR_BASE) > 0.02):
+        checkMag()
+        checkUVLight()
