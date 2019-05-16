fe57bff924fcfcf3aa45f5a96411458b69b277f3
Hussain Abbasi <H_abbasi97@hotmail.com>
2019-05-16 16:13:18 +0400

    pycharm integration

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 monitored_folder = 'C:\\Users\\H_Abb\\Desktop\\Space_Hackathon'
-mag, tempir, uvlight, magx, magy, magz,dist = [], [], [], [], [], [],[]
+mag, tempir, uvlight, magx, magy, magz, dist = [], [], [], [], [], [], []
 
 MAG_BASE = 3.22389238
 TEMP_IR_BASE = 25.3
@@ -18,18 +18,19 @@ print('UV_LIGHT_INDEX_BASE: ', UV_LIGHT_INDEX_BASE)
 
 
 def checkMag():
-    if(len(tempir) > 5 and ((sum(mag[-5:]) / 5) - MAG_BASE) / MAG_BASE > 0.01):
+    if len(tempir) > 5 and ((sum(mag[-5:]) / 5) - MAG_BASE) / MAG_BASE > 0.01:
         print('Solar winds detected')
 
 
 def checkUVLight():
-    if(len(tempir) > 5 and ((sum(uvlight[-5:]) / 5) - UV_LIGHT_INDEX_BASE) / UV_LIGHT_INDEX_BASE > 0.01):
+    if len(tempir) > 5 and ((sum(uvlight[-5:]) / 5) - UV_LIGHT_INDEX_BASE) / UV_LIGHT_INDEX_BASE > 0.01:
         print('Object detected')
 
-def lineGraph(xlabel,ylabel,title,X,y):
-    plt.scatter(X,y,color='red')
-    plt.plot(X,y,color = 'blue')
-    plt.gca().set_ylim([-20,120])
+
+def lineGraph(xlabel, ylabel, title, X, y):
+    plt.scatter(X, y, color='red')
+    plt.plot(X, y, color='blue')
+    plt.gca().set_ylim([-20, 120])
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
@@ -38,6 +39,7 @@ def lineGraph(xlabel,ylabel,title,X,y):
 
 class FolderMonitor(FileSystemEventHandler):
     def on_modified(self, event):
+        print(event)
         dataset = pd.read_csv('data.csv')
         magx.append(dataset.iloc[-1:, :1].values.tolist()[0][0])
         magy.append(dataset.iloc[-1:, 1:2].values.tolist()[0][0])
@@ -51,12 +53,12 @@ class FolderMonitor(FileSystemEventHandler):
         del tempir[len(dataset):]
         del uvlight[len(dataset):]
         del dist[len(dataset):]
-        if(len(tempir) > 5 and (abs((sum(tempir[-5:]) / 5) - TEMP_IR_BASE) / TEMP_IR_BASE) > 0.02):
+        if len(tempir) > 5 and (abs((sum(tempir[-5:]) / 5) - TEMP_IR_BASE) / TEMP_IR_BASE) > 0.02:
             checkMag()
             checkUVLight()
-        if(len(dist)>5):
+        if len(dist) > 5:
             print('hi')
-            lineGraph('Distance (m)','Temperature (C)','IR output',dist,tempir)
+            lineGraph('Distance (m)', 'Temperature (C)', 'IR output', dist, tempir)
 
 
 observer = Observer()
